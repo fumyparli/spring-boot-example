013a9562c71cd8237e13eb554a3a96dbe0ef5794
fumyparli <qkskskn@naver.com>
2021-04-11 16:15:25 +0900

    Add code

--- src/main/java/ga/fumyparli/e1/repository/MemoryMemberRepository.java
+++ src/main/java/ga/fumyparli/e1/repository/MemoryMemberRepository.java
@@ -1,4 +1,36 @@
 package ga.fumyparli.e1.repository;
 
-public class MemoryMemberRepository {
+import ga.fumyparli.e1.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+
+    public Member save(Member member) {
+        member.setId((long) store.size());
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
