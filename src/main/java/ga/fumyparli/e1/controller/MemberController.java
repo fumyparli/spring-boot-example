7a6db27003b1e56e623dd7a50106ff1fc692f2e2
fumyparli <qkskskn@naver.com>
2021-04-12 15:55:11 +0900

    Add front code and sql code

--- src/main/java/ga/fumyparli/e1/controller/MemberController.java
+++ src/main/java/ga/fumyparli/e1/controller/MemberController.java
@@ -1,15 +1,57 @@
 package ga.fumyparli.e1.controller;
 
+import ga.fumyparli.e1.domain.Member;
 import ga.fumyparli.e1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
+    // Constructor DI : 이것이 젤 좋다.
+
     private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
+//    //setter DI 단점: bean을 바꿀일이 없는데 setter를 public으로 열어줘야한다.
+//    private MemberService memberService;
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+//    //field DI 단점: 수정이 불가능하다.
+//    @Autowired
+//    private MemberService memberService;
 }
